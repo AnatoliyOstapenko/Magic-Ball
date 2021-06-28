c4a17a96586cf66ff0d1d9b73a9e87f3c2b22c29
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2021-06-28 20:05:53 +0300

    the main assets added
    app is completed to use

--- MagicBall/ViewController.swift
+++ MagicBall/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var magicBallView: UIImageView!
+    @IBOutlet weak var suggestLabel: UILabel!
+    var ballArray = [#imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball5")]
+    var suggestion = ["Now, you have my answer", "It's your day", "Don't give up!", "I always tell the truth" , "Hmm, you are luky"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        suggestLabel.adjustsFontSizeToFitWidth = true
+        suggestLabel.minimumScaleFactor = 0.5
+        suggestLabel.text = "Push the button"
     }
 
-
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        magicBallView.image = ballArray.randomElement()
+        suggestLabel.text = suggestion.randomElement()
+        //let unwrappedSuggestion = suggestion.randomElement()
+    }
+    
 }
 
